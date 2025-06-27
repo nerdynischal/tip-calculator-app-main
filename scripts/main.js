c6e588e58e227fc722719adf1a7bc41b626e8240
nerdynischal <nerdynischal@gmail.com>
2025-06-27 16:10:04 +0100

    Add reset button functionality

--- scripts/main.js
+++ scripts/main.js
@@ -22,12 +22,18 @@ function roundTwoDecimalPlaces(num) {
   return num.toFixed(2);
 }
 
+let tipAmountDisplay = document.querySelector(".form__tip-value");
+let totalAmountDisplay = document.querySelector(".form__total-value");
+
 //calculate the amounts to display
 function calculateEverything() {
   const totalTip = calculateTotalTip(totalBill, tipPercent);
   const tipPerPerson = calculateTipPerPerson(totalTip, totalPeople);
   const billPerPerson = calculateBillPerPerson(totalBill, totalPeople);
   const totalPerPerson = calculateTotalPerPerson(tipPerPerson, billPerPerson);
+
+  tipAmountDisplay.textContent = "$" + roundTwoDecimalPlaces(tipPerPerson);
+  totalAmountDisplay.textContent = "$" + roundTwoDecimalPlaces(totalPerPerson);
 
   console.log("Total tip per person: " + roundTwoDecimalPlaces(tipPerPerson));
   console.log("Total per person: " + roundTwoDecimalPlaces(totalPerPerson));
@@ -48,18 +54,30 @@ tipRadioInput.forEach((option) => {
 customTipInput.addEventListener("click", uncheckRadioOptions);
 customTipInput.addEventListener("keyup", assignTip);
 
+const errorMsgs = document.querySelectorAll(".form__error-msg");
+
 function assignBillValue() {
   if (validateInputs(this.value) == false) {
+    errorMsgs[0].style.display = "initial";
+    billInput.style.borderColor = "var(--red-500)";
     return;
   }
+  errorMsgs[0].style.display = "none";
+  billInput.style.borderColor = "var(--grey-50)";
+
   totalBill = this.value;
   calculateEverything();
 }
 
 function assignTotalPeople() {
   if (validateInputs(this.value) == false) {
+    errorMsgs[1].style.display = "initial";
+    peopleInput.style.borderColor = "var(--red-500)";
     return;
   }
+  errorMsgs[1].style.display = "none";
+  peopleInput.style.borderColor = "var(--grey-50)";
+
   totalPeople = this.value;
   calculateEverything();
 }
@@ -83,8 +101,14 @@ function validateInputs(input) {
     console.log("Please enter a number");
     return false;
   }
-  if (input < 0) {
+  if (input <= 0) {
     console.log("Please enter a number greater than 1");
     return false;
   }
 }
+
+const resetButton = document.querySelector(".form__reset-button");
+resetButton.addEventListener("click", () => {
+  tipAmountDisplay.textContent = "$0.00";
+  totalAmountDisplay.textContent = "$0.00";
+});
